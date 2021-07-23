5666d0a97eb0390b02e74e1d49c6d0a6dfda5f8d
Renan Lima <renan.lima.softwares@gmail.com>
2021-07-23 17:11:21 -0300

    add node modules

---
+++ javascript/node/modulo02/index.js
@@ -0,0 +1,6 @@
+const multiple = require('./app')
+
+console.log(multiple(2, 2))
+
+console.log(__filename) // /home/renan/cursos/otavio/js-ts/javascript/node/modulo02/index.js
+console.log(__dirname) // /home/renan/cursos/otavio/js-ts/javascript/node/modulo02
