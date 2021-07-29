2d3f5845e71c61073cbe339d98d0dd5326f0febc
Renan Lima <renan.lima.softwares@gmail.com>
2021-07-29 02:45:41 -0300

    add crud to user

--- javascript/node/api-rest/src/controllers/UserController.js
+++ javascript/node/api-rest/src/controllers/UserController.js
@@ -5,9 +5,89 @@ class UserController {
     try {
       const novoUser = await User.create(req.body)
 
-      res.status(200).json(novoUser)
+      const user = {
+        ...novoUser.dataValues,
+        password: novoUser.dataValues.password_hash // gambiarra para sobrescrever a senha
+      }
+
+      return res.status(200).json(user) // poderia retornar novoUser direto aqui
     } catch (e) {
-      res.status(401).json({
+      return res.status(401).json({
+        erros: e.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      return res.json(users)
+    } catch (e) {
+      return res.status(401).json(null)
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params
+      const user = await User.findByPk(id)
+      return res.json(user)
+    } catch (e) {
+      return res.status(401).json(null)
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado']
+        })
+      }
+
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe']
+        })
+      }
+
+      const novosDados = await user.update(req.body)
+
+      return res.json(novosDados)
+    } catch (e) {
+      return res.status(401).json({
+        erros: e.errors.map(err => err.message)
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado']
+        })
+      }
+
+      const user = await User.findByPk(id)
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe']
+        })
+      }
+
+      await user.destroy()
+
+      return res.json('Usuário deletado.')
+    } catch (e) {
+      return res.status(401).json({
         erros: e.errors.map(err => err.message)
       })
     }
