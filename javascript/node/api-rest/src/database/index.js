5b3c8fb41d51b4e039af3c82484861987e9cb4dc
Renan Lima <renan.lima.softwares@gmail.com>
2021-08-01 02:54:25 -0300

    add upload na base de dados

--- javascript/node/api-rest/src/database/index.js
+++ javascript/node/api-rest/src/database/index.js
@@ -3,9 +3,11 @@ import databaseConfig from '../config/database'
 
 import Aluno from '../models/Aluno'
 import User from '../models/User'
+import Foto from '../models/Foto'
 
-const models = [Aluno, User]
+const models = [Aluno, User, Foto]
 
 const connection = new Sequelize(databaseConfig)
 
 models.forEach(model => model.init(connection)) // Sequelize.init([esseParam]) do model.
+models.forEach(model => model.associate && model.associate(connection.models)) // pra verificar se tem associação.
