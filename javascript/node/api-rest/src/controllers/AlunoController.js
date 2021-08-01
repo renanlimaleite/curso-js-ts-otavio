9cfa1006bbf8060e71b91e2d4f319ea2b848d892
Renan Lima <renan.lima.softwares@gmail.com>
2021-08-01 03:39:12 -0300

    add config para arquivos estaticos

--- javascript/node/api-rest/src/controllers/AlunoController.js
+++ javascript/node/api-rest/src/controllers/AlunoController.js
@@ -8,7 +8,7 @@ class AlunoController {
       order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
       include: {
         model: Foto,
-        attributes: ['filename', 'originalname']
+        attributes: ['url', 'filename', 'originalname']
       }
     })
     res.json(alunos)
@@ -41,7 +41,7 @@ class AlunoController {
         order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
         include: {
           model: Foto,
-          attributes: ['filename', 'originalname']
+          attributes: ['url', 'filename', 'originalname']
         }
       })
 
