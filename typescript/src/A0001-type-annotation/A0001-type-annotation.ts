2b9dabea13a884088bf356a795a0fae9aebdf6b7
Renan Lima <renan.lima.softwares@gmail.com>
2021-09-07 08:52:20 -0300

    add about type annotation

---
+++ typescript/src/A0001-type-annotation/A0001-type-annotation.ts
@@ -0,0 +1,29 @@
+/* eslint-disable */
+
+let nome: string = 'Renan'
+let idade: number = 24
+let adulto: boolean = true
+let simbolo: symbol = Symbol('qualquer-symbol')
+// let big: bigint = 10
+
+// Arrays
+let arrayDeNumeros: Array<number> = [1, 2, 3]
+let arrayDeNumeros2: number[] = [1, 2, 3]
+
+let arrayDeString: Array<string> = ['Renan', 'Lima']
+let arrayDeString2: string[] = ['Renan', 'Lima']
+
+
+// Objetos
+let pessoa: { nome: string, idade: number, adulto?: boolean } = {
+  nome: 'Renan',
+  idade: 20,
+  adulto: true
+}
+
+// Função
+function soma(x: number, y: number) {
+  return x + y
+}
+
+const result = soma(2, 2)
--- typescript/src/index.ts
+++
@@ -1,3 +0,0 @@
-const name = 'renan'
-
-const sobrenome = 'Leite'
